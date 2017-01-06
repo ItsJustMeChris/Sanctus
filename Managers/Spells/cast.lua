f889766f43e11cb1b1c75033d22ada0cdeef064e
ItsJustMeChris <Chris@chris.com>
2017-01-06 01:35:34 -0500

    Update
    Remove debug again.

--- Managers/Spells/cast.lua
+++ Managers/Spells/cast.lua
@@ -1,6 +1,6 @@
 function cast(spell, target)
     local spellname = GetSpellInfo(spell)
-            CastSpellByName(spellname,target)
+        CastSpellByName(spellname,target)
 end
 
 function castable(cd, tar)
@@ -18,16 +18,13 @@ function distcheck(spell, targ)
     local maxdist = select(6, GetSpellInfo(spell))
     if ObjectExists(targ) then
         if distance(targ, "player") >= mindist + UnitBoundingRadius(targ) and distance(targ, "player") <= maxdist + UnitBoundingRadius(targ) then
-            Debug("Spell: "..spell..", Distance Minimum: "..mindist..", Distance Maximum: "..maxdist..", Casting: True")
             return true
         elseif mindist == 0 and maxdist == 0 then
             if distance(targ, "player") >= 0 and distance(targ, "player") <= 25 then
-                Debug("Spell: "..spell..", Distance Minimum: "..mindist..", Distance Maximum: "..maxdist..", Casting: True")
                 return true
             end
         elseif mindist == 0 and maxdist > 0 then
             if distance(targ, "player") >= 0 and distance(targ, "player") <= maxdist then
-                Debug("Spell: "..spell..", Distance Minimum: "..mindist..", Distance Maximum: "..maxdist..", Casting: True")
                 return true
             end
         else
